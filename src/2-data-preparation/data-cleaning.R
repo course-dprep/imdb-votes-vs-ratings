75910cd152309009c094ca2f9e0ced91dc8be050
EefjevdSanden <e.j.m.vdrsanden@tilburguniversity.edu>
2025-10-02 23:15:52 +0200

    Integrated code comments, revised logistic regression, revised MASS library error

--- src/2-data-preparation/data-cleaning.R
+++ src/2-data-preparation/data-cleaning.R
@@ -1,6 +1,4 @@
 print('start importing data for clean')
-# In this directory, you will keep all source code files relevant for 
-# preparing/cleaning your data.
 
 # Load packages (subfolder-relative)
 source("../1-raw-data/loading-packages.R")
@@ -16,12 +14,14 @@ ratings <- read_tsv("../../data/title.ratings.tsv.gz", na = "\\N",
 print('imported data for clean')
 
 # TRANSFORMATION
-print('start transformation clean')
-# Merge datasets on tconst
+#Merge the datasets based on the common identifier (tconst)
 imdb_raw <- basics %>%
   inner_join(ratings, by = "tconst")
 
-# Cast types and filter to movies/series
+#Type casting and selection of relevant title types
+# - Convert startYear into integer
+# - Classify titles into simplified categories: movie, series, other
+# - Filter to keep only movies and series
 imdb_raw <- imdb_raw %>%
   mutate(
     startYear = suppressWarnings(as.integer(startYear)),
@@ -30,15 +30,28 @@ imdb_raw <- imdb_raw %>%
   ) %>%
   filter(type %in% c("movie", "series"))
 
-# Remove duplicates
+#Remove duplicates based on the identifier - i.e., if any titletype appears more than once in the dataset. 
 imdb_dedup <- imdb_raw %>%
   distinct(tconst, .keep_all = TRUE)
 
-# Filter by minimum votes
+#Filter out titles with very few votes to reduce noise
+"Inspect numVotes variable; median is 49, mean is 2746 votes. This means that the numVotes is heavlity skewed to the right; ie, many titles have just a few votes. Although number of votes (numVotes) presents the variable of interest of our research question, we have decided to only keep titles which have at least 20 votes. The reason is to reduce noise: titles with just one or two reviews ma have ratings which are not representative for the movie, whilst ratings by several people may reduce this noise whilst keeping more extreme opinions of a small group." 
+imdb_dedup %>%
+  summarise(
+    min_votes = min(numVotes, na.rm = TRUE),
+    max_votes = max(numVotes, na.rm = TRUE),
+    median_votes = median(numVotes, na.rm = TRUE),
+    mean_votes = mean(numVotes, na.rm = TRUE),
+    sd_votes = sd(numVotes, na.rm = TRUE)
+  )
 imdb_clean <- imdb_dedup %>%
   filter(numVotes >= 20)
 
-# Map genres into families
+# Map individual genres into broader genre families
+# 1.Remove rows with missing or empty genres. Note that this is necessary because data-filing processes do not make sense here; one cannot infer the genre of a title based on the genre of the title that was realized in the same year (or based on another variable).
+# 2.Split multiple genres into separate rows
+# 3.Assign each genre to a family: 'Escapist'(fantasy, romance, action, adventure, animation, family) or 'Heavy' (drama, thriller, biography, crime, documentary and heavy) or mixed (both escapist and heavy)
+# 4.Summarize back to one row per title, combining genre families
 genre_map <- imdb_clean %>%
   filter(!is.na(genres), genres != "") %>%
   select(tconst, genres) %>%
@@ -58,15 +71,14 @@ genre_map <- imdb_clean %>%
     .groups = "drop"
   )
 
-# Final enrichment
+#Merge new genre categories back into dataset
+"Ensure the imdb_clean does not have empty genre rows either; enrich with extra genre features"
 imdb_clean <- imdb_clean %>% 
   filter(!is.na(genres), genres != "")
-
 imdb_enriched <- imdb_clean %>%
   left_join(genre_map, by = "tconst") %>%
   select(tconst, titleType, type, startYear, genres, genre_family,
          averageRating, numVotes)
-print('cleaned transformed')
 
 # OUTPUT (to root-level data/clean)
 dir.create("../../data/clean", showWarnings = FALSE, recursive = TRUE)
--- src/3-analysis/analysis.R
+++ src/3-analysis/analysis.R
@@ -2,20 +2,20 @@
 source("../1-raw-data/loading-packages.R")
 
 #Load imdb dataset 
-imdb_analysis <- read_csv("../../data/clean/imdb_analysis.csv")
+imdb_analysis_main <- read_csv("../../data/clean/imdb_analysis.csv")
 
 #Keep only those observations with an exclusive genre
-imdb_analysis <- imdb_analysis %>%
+imdb_analysis_main <- imdb_analysis_main %>%
   filter(genre_family == "Escapist" | genre_family == "Heavy")
 
 #Model 1: regress rating (averageRating) on the log number of votes (log_votes), controlling for period
-model_linear <- lm(averageRating ~ log_votes + period, data = imdb_analysis)
+model_linear <- lm(averageRating ~ log_votes + period, data = imdb_analysis_main)
 summary(model_linear)
 tidy(model_linear)
 print('Model 1 made')
 
 #Model 2: regress rating (averageRating) on the log number of votes (log_votes), the quadratic log number of votes (log_votes2), controlling for period
-model_quadratic <- lm(averageRating ~ log_votes + log_votes2 + period, data = imdb_analysis)
+model_quadratic <- lm(averageRating ~ log_votes + log_votes2 + period, data = imdb_analysis_main)
 summary(model_quadratic)
 tidy(model_quadratic)
 print('Model 2 made')
@@ -25,72 +25,51 @@ anova(model_linear, model_quadratic)
 
 #Model 3 (subQ 1): Interaction with the genre variable
 model_interaction_genre <- lm( averageRating ~ log_votes + log_votes2 + genre_family +
-  log_votes*genre_family + log_votes2*genre_family, data = imdb_analysis)
+  log_votes*genre_family + log_votes2*genre_family, data = imdb_analysis_main)
 summary(model_interaction_genre)
 tidy(model_interaction_genre)
 print('Model 3 made')
-
-#-------- this line gives error -------------
-anova(model_quadratic, model_interaction_genre)
-#---------------------------------------------
 
 #Model 4 (subQ 2): Interaction with the type (movie vs film) variable
 model_interaction_type <- lm(
   averageRating ~ log_votes + log_votes2 + type +
     log_votes*type + log_votes2*type,
-  data = imdb_analysis)
+  data = imdb_analysis_main)
 
 print('Model 4 made')
 summary(model_interaction_type)
 tidy(model_interaction_type)
 
-
-anova(model_quadratic, model_interaction_type)
-
-#Model 5: For managerial practicallity, regress rating categories (averageRating) on the log number of votes (log_votes), the quadratic log number of votes (log_votes2), controlling for period. This is a logistic regression, as both the IV and DV are non-continuous.
-model_logistic_categories <- polr(rating_category ~ log_votes + I(log_votes2), 
-                  data = imdb_analysis, Hess = TRUE)
-print('Model 5 made')
-
-summary(model_logistic_categories)
-tidy(model_logistic_categories)
-
 #Visualization
 #Model 1 & 2 
-Model_1_2 <- ggplot(imdb_analysis, aes(x = log_votes, y = averageRating)) +
+Model_1_2 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating)) +
   geom_point(alpha = 0.1) +   # raw data
   geom_smooth(method = "lm", formula = y ~ x, color = "blue") +   # linear
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "red") +   # quadratic
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "violet") +   # quadratic
   labs(title = "Average Rating vs Number of votes, linear and quadratic log number of votes",
        x = "Log(Number of Votes)", y = "Average Rating")
 
 #Model 3 
-Model_3 <- ggplot(imdb_analysis, aes(x = log_votes, y = averageRating, color = genre_family)) +
+Model_3 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating, color = genre_family)) +
   geom_point(alpha = 0.1) +
   geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE) +
   labs(title = "Rating vs Votes (logged) by Genre (Heavy versus Escapist)",
        x = "Log(Number of Votes)", y = "Average Rating") +
   facet_wrap(~ genre_family)
+print(Model_3)
+
 
 #Model 4
-Model_4 <- ggplot(imdb_analysis, aes(x = log_votes, y = averageRating, color = type)) +
+Model_4 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating, color = type)) +
   geom_point(alpha = 0.1) +
   geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE) +
   labs(title = "Rating vs Votes (logged) by content type (movie vs serie)",
        x = "Log(Number of Votes)", y = "Average Rating") +
   facet_wrap(~ type)
 
-#Model 5 
-Model_5 <- imdb_analysis %>%
-  mutate(vote_bin = cut(log_votes, breaks = 20)) %>%
-  group_by(vote_bin, rating_category) %>%
-  summarise(n = n(), .groups = "drop") %>%
-  group_by(vote_bin) %>%
-  mutate(prop = n / sum(n)) %>%
-  ggplot(aes(x = vote_bin, y = prop, fill = rating_category)) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "Distribution of Rating Categories across Log(Votes)",
-       x = "Log(Votes) (binned)", y = "Proportion")
 
 print('Visualization of all models done')
 
+  
+
+#NOTE, need codes for saving the respective plots
