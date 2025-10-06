3191fb448faf1e1d346fc7a466868e255295df3b
Sanne Wielders <sannewielders@ww020268.uvt.nl>
2025-10-06 10:56:31 +0200

    update loading packages only to library and added an installing packages

--- src/1-raw-data/loading-packages.R
+++ src/1-raw-data/loading-packages.R
@@ -1,26 +1,17 @@
+### OPTION 1
 # List of required packages
 print('start loading packages')
-packages <- c(
-  "readr",
-  "tidyverse",
-  "dplyr",
-  "tidyr",
-  "ggplot2",
-  "here",
-  "vroom",
-  "broom",
-  "ordinal"
-)
 
-# Install any missing packages
-installed <- rownames(installed.packages())
-for (pkg in packages) {
-  if (!pkg %in% installed) {
-    install.packages(pkg, dependencies = TRUE)
-  }
-}
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+library(vroom)
+library(broom)
+library(ordinal)
 
-# Load packages
-lapply(packages, library, character.only = TRUE)
 
 print('packages loaded')
+
