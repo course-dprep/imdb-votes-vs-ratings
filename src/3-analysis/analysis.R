09dcf82f46ae7a4288deda2f97b2ab90f7a8726a
EefjevdSanden <e.j.m.vdrsanden@tilburguniversity.edu>
2025-10-08 15:42:13 +0200

    add regression output into report

--- src/3-analysis/analysis.R
+++ src/3-analysis/analysis.R
@@ -50,28 +50,32 @@ Model_1_2 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating)) +
        x = "Log(Number of Votes)", y = "Average Rating")
 print('Model 1,2 done')
 
-#Model 3 
-Model_3 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating, color = genre_family)) +
-  geom_point(alpha = 0.1) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE) +
+# Model 3 
+Model_3 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating)) +
+  geom_point(alpha = 0.1, color = "gray50") +  
+  geom_smooth(aes(color = genre_family),       
+              method = "lm", formula = y ~ poly(x, 2), se = FALSE, size = 1) +
   labs(title = "Rating vs Votes (logged) by Genre (Heavy versus Escapist)",
-       x = "Log(Number of Votes)", y = "Average Rating") +
-  facet_wrap(~ genre_family)
+       x = "Log(Number of Votes)", y = "Average Rating",
+       color = "Genre") +
+  facet_wrap(~ genre_family) +
+  theme_minimal()
 print('Model 3 done')
 
 
-#Model 4
-Model_4 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating, color = type)) +
-  geom_point(alpha = 0.1) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE) +
-  labs(title = "Rating vs Votes (logged) by content type (movie vs serie)",
-       x = "Log(Number of Votes)", y = "Average Rating") +
-  facet_wrap(~ type)
+# Model 4
+Model_4 <- ggplot(imdb_analysis_main, aes(x = log_votes, y = averageRating)) +
+  geom_point(alpha = 0.1, color = "gray50") +  
+  geom_smooth(aes(color = type),               
+              method = "lm", formula = y ~ poly(x, 2), se = FALSE, size = 1) +
+  labs(title = "Rating vs Votes (logged) by Content Type (Movie vs Series)",
+       x = "Log(Number of Votes)", y = "Average Rating",
+       color = "Content Type") +
+  facet_wrap(~ type) +
+  theme_minimal()
 print('Model 4 done')
 
 print('Visualization of all models done')
-
-  
 
 # creating our gen/output folder
 dir.create("../../gen/output", recursive = TRUE, showWarnings = FALSE)
@@ -81,3 +85,25 @@ ggsave("../../gen/output/model1_2.png", Model_1_2, width = 8, height = 6)
 ggsave("../../gen/output/model3.png",   Model_3,   width = 8, height = 6)
 ggsave("../../gen/output/model4.png",   Model_4,   width = 8, height = 6)
 print('models saved in gen/output')
+
+# Install once
+install.packages("modelsummary")
+
+# Load
+library(modelsummary)
+
+# Save as png table
+models <- list(
+  "Linear" = model_linear,
+  "Quadratic" = model_quadratic,
+  "Genre Interaction" = model_interaction_genre,
+  "Type Interaction" = model_interaction_type
+)
+
+modelsummary(
+  models,
+  output = "../../gen/output/regression_models.md",
+  title = "Regression Models: Ratings vs Votes",
+  stars = TRUE
+)
+
